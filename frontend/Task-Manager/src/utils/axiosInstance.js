f939f0d5129231b0e4464ed447002573fd83ddbd
palak-sh3 <palak336pyl@gmail.com>
2025-09-29 16:45:36 +0530

    add versel

--- frontend/Task-Manager/src/utils/axiosInstance.js
+++ frontend/Task-Manager/src/utils/axiosInstance.js
@@ -1,12 +1,12 @@
 import axios from "axios";
+import { BASE_URL } from "./apiPaths";
 
 const axiosInstance = axios.create({
+  baseURL: BASE_URL,
   timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
+  headers: { "Content-Type": "application/json" },
 });
+
 
 // Attach token if present
 axiosInstance.interceptors.request.use(
