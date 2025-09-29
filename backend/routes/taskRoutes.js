31859fad35e4cdbeafe73e63412e784ef9561a82
palak-sh3 <palak336pyl@gmail.com>
2025-09-29 15:37:48 +0530

    Initial commit

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { protect, adminOnly } = require("../middleware/authMiddleware");
+const { getDashboardData, getUserDashboardData, getTasks, getTaskById, createTask, updateTask, deleteTask, updateTaskStatus, updateTaskChecklist } = require('../controllers/taskController');
+
+const router = express.Router();
+
+//Task Management Routes
+router.get("/dashboard-data", protect, getDashboardData);
+router.get("/user-dashboard-data", protect, getUserDashboardData);
+router.get("/", protect, getTasks); // Get all tasks (Admin: all, User: assigned)
+router.get("/:id", protect, getTaskById); // Get task by ID
+router.post("/", protect, adminOnly, createTask); // Create a new task(Admin only)
+router.put("/:id", protect, updateTask); // Update a task (Admin: any, User: assigned)
+router.delete("/:id", protect, adminOnly, deleteTask); // Delete a task (Admin only)
+router.put("/:id/status", protect, updateTaskStatus); // Update a task status
+router.put("/:id/todo", protect, updateTaskChecklist); // Update a task checklist
+
+module.exports = router;
+
